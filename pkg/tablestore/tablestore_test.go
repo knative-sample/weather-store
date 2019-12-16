3d05956d27d66bdb79dfb672a65854cc2fb62e38
yuanyi.lp <yuanyi.lp@alibaba-inc.com>
2019-12-16 13:22:32 +0800

    feature(weather-store): update load city test case

--- pkg/tablestore/tablestore_test.go
+++ pkg/tablestore/tablestore_test.go
@@ -1,20 +1,21 @@
 package tablestore
 
 import (
+	"strings"
 	"testing"
 
 	"fmt"
 
 	"github.com/aliyun/aliyun-tablestore-go-sdk/tablestore"
+	"github.com/knative-sample/weather-store/pkg/utils"
 	"github.com/knative-sample/weather-store/pkg/weather"
 )
 
-func FakeInitClient() *TableClient {
-	endpoint := "https://xxx.cn-beijing.ots.aliyuncs.com" //实例访问地址
-	tableName := "weather"                                // 表名
-	instanceName := "knative-weather"                     // 实例名
-	accessKeyId := "xx"                                   // AccessKey ID
-	accessKeySecret := "xx"                               //Access Key Secret
+func FakeInitClient(tableName string) *TableClient {
+	endpoint := "https://xx.cn-beijing.ots.aliyuncs.com" //实例访问地址
+	instanceName := "knative-weather"                    // 实例名
+	accessKeyId := "xx"                                  // AccessKey ID
+	accessKeySecret := "xx"                              //Access Key Secret
 	client := tablestore.NewClient(endpoint, instanceName, accessKeyId, accessKeySecret)
 	return &TableClient{
 		tableName: tableName,
@@ -22,7 +23,7 @@ func FakeInitClient() *TableClient {
 	}
 }
 func TestStore(t *testing.T) {
-	c := FakeInitClient()
+	c := FakeInitClient("weather")
 	ct := weather.Cast{
 		Date:         "2019-09-24",
 		Week:         "2",
@@ -46,5 +47,34 @@ func TestStore(t *testing.T) {
 	err := c.Store(f)
 	if err != nil {
 		fmt.Println(err.Error())
+	}
+}
+
+func TestStoreCity(t *testing.T) {
+	cityCodes := strings.Split(utils.CityInfo, "\n")
+	client := FakeInitClient("city")
+	pre := ""
+	for _, cityCode := range cityCodes {
+		if cityCode == "" {
+			continue
+		}
+		items := strings.Split(cityCode, ",")
+		ci := weather.CityInfo{IsCity: "false"}
+		ci.Name = items[0]
+		ci.Adcode = items[1]
+		if len(items) == 3 {
+			ci.Citycode = items[2]
+			if ci.Citycode != "" && pre != ci.Citycode {
+				ci.IsCity = "true"
+			}
+			pre = ci.Citycode
+		}
+
+		err := client.StoreCity(ci)
+		if err != nil {
+			fmt.Errorf("StoreCity: %s", err.Error())
+			continue
+		}
+		fmt.Println("load: ", ci.Name)
 	}
 }
